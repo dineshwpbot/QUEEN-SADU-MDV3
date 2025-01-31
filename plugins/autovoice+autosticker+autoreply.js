67ef03c85c6a3afe49f21ea69412691de49772d2
dineshwpbot <herathmadusankadinesh@gmail.com>
2025-01-31 14:51:36 +0530

    Update autovoice+autosticker+autoreply.js

--- plugins/autovoice+autosticker+autoreply.js
+++ plugins/autovoice+autosticker+autoreply.js
@@ -60,6 +60,24 @@ async (conn, mek, m, { from, body, isOwner }) => {
     }                
 });
 
+//auto_voice
+cmd({
+  on: "body"
+},    
+async (conn, mek, m, { from, body, isOwner }) => {
+    const filePath = path.join(__dirname, '../my_data/autovideo.json');
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    for (const text in data) {
+        if (body.toLowerCase() === text.toLowerCase()) {
+            
+            if (config.AUTO_VIDEO === 'true') {
+                //if (isOwner) return;        
+                await conn.sendPresenceUpdate('recording', from);
+                await conn.sendMessage(from, { video: { url: data[text] }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
+            }
+        }
+    }                
+});
 
 //fake recording
 cmd({
